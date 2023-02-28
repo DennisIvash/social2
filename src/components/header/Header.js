dc506f3cf2e26070b45ed2010385441f297aefce
Dennis Ivash <dripdennis420@gmail.com>
2023-02-28 12:45:56 -0500

    comment

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import logo from '../../img/logo.svg'
+import './header.css'
+
+
+function Header() {
+    return (
+      <div className="header">
+        <img src={logo} alt="" />
+      </div>
+      
+    )
+  }
+
+export default Header;
+  
---
+++ src/components/profile/Profile.js
@@ -0,0 +1,27 @@
+import './Profile.css'
+import ava from '../../img/elon.jpg'
+import avamin from '../../img/elon_mini.jpg'
+
+
+function Profile() {
+    return (
+      <div className="profile">
+        <div className="me">
+          <img src={ava} alt="profile-photo" />
+          <p>Elon Musk</p>
+        </div>
+        <div className="posts">
+          <input type="text" placeholder='Enter The Post'/>
+          <button>Add Post</button>
+          <div className="post">
+            <img src={avamin} alt="mini-profile-photo" />
+            <span>Elon Musk</span>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod earum recusandae labore fugit quasi. Culpa sunt, exercitationem quia eum, voluptatibus mollitia vitae perferendis libero maiores tenetur veritatis quasi quam saepe?</p>
+          </div>
+        </div>
+      </div>
+      
+    )
+  }
+
+export default Profile;
